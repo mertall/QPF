336d54a7ed25c7f3758df19ceb7014c396401a81
Mridul Sarkar <42987995+mridulsar@users.noreply.github.com>
2020-06-22 00:38:42 -0700

    Update preprocess_DB.py

--- preprocess_DB.py
+++ preprocess_DB.py
@@ -4,11 +4,13 @@ def preprocess():
     uf_DB,names=histine_Data.grab_db()
     X=np.c_[uf_DB]
     data = []
-    #for i in range(len(names)):
-        #if i != len(names):
-            #data.append(names[i] + ' ' + str(X[i]) + '; ' + names[i+1] + ' ' + str(X[i+1]))
-        #else:
-            #break
+    x_naught = X[0]
+    data.append(names[0] + ' ' + str(X[0]) + '; ' + names[1] + ' ' + str(X[1])
+    for i in np.zeros:
+        if i != 0 or 1:
+            data.append(names[i-1] + ' ' + str(X[i-1]) + '; ' + names[i] + ' ' + str(X[i]))
+        else:
+            continue
     print(names)
     print(X)
 print(preprocess())
